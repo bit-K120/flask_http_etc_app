b2809aed330ba420ac1c8bef05ea0486bae141cc
bit-K120 <kayugen105@gmail.com>
2023-10-07 12:09:32 +0900

    hello and member.html created

---
+++ list_app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/members')
+def memebers():
+    members = ["Bob","Tom","Ken"]
+    return render_template('members.html', members = members)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
+ 
